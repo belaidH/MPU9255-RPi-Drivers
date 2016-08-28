c61b55f39ba0a68672cf086c690ff4095cfb413d
Brenton Sutherland <brentonsutherland311@gmail.com>
2016-08-27 21:58:48 -0300

    Updated example file to include more documentation

--- examples/AccelGyro.cpp
+++ examples/AccelGyro.cpp
@@ -4,19 +4,16 @@
 int main() 
 {
     MPU9255 imu;
-    std::cout << "Gyroscope" << std::endl;
-    for(int i = 0; i < 10; i++)
-    {
-        auto rates = imu.getRotationRates();
-        std::cout << "X: " << rates.x << " Y: " << rates.y << " Z: " << rates.z << std::endl;
-    }
 
+    // Get the rotational rates from the gyroscope in units of degrees/second
+    std::cout << "Gyroscope" << std::endl;
+    auto rates = imu.getRotationRates();
+    std::cout << "X: " << rates.x << " Y: " << rates.y << " Z: " << rates.z << std::endl;
+
+    // Get the forces measured from the accelerometer in units of g's (1g of gravity is nominal Earth gravity)
     std::cout << "Accelerometer" << std::endl;
-    for(int i = 0; i < 10; i++)
-    {
-        auto forces = imu.getAccelerations();
-        std::cout << "X: " << forces.x << " Y: " << forces.y << " Z: " << forces.z << std::endl;
-    }
+    auto forces = imu.getAccelerations();
+    std::cout << "X: " << forces.x << " Y: " << forces.y << " Z: " << forces.z << std::endl;
 
     return 0;
 }
